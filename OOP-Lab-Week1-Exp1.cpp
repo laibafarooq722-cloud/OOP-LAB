7e1f0b7f0be60723349f6b6805aca5c1f2c6bef6
laibaFarooq <laibafarooq722@gmail.com>
2026-06-07 19:25:53 +0500

    Add files via upload

---
+++ OOP-Lab-Week1-Exp1.cpp
@@ -0,0 +1,33 @@
+// OOP-Lab-Week1-Exp1.cpp 
+#include <iostream>
+using namespace std;
+struct Student {
+    int studentID;
+    char* studentName;
+    char* courseCode;
+    char* courseName;
+    int courseMarks;
+};
+int main() {
+    int id, marks;
+    char name[50], course_code[20], course_name[50];
+cout << "Get Values from User"<<endl;
+cout << "Enter Roll Number: ";
+cin >> id;
+cout << "Enter Student Name: ";
+ cin >> name;
+ cout << "Enter Course : ";
+ cin >> course_code;
+ cout << "Enter Course Name: ";
+ cin >> course_name;
+ cout << "Enter Course Marks: ";
+ cin >> marks;
+Student s1 = { id,name,course_code,course_name,marks };
+    cout << "Display Stored Values"<<endl;
+    cout << "Student ID: " << s1.studentID << endl;
+    cout << "Student Name: " << s1.studentName << endl;
+    cout << "Course Code: " << s1.courseCode << endl;
+    cout << "Course Name: " << s1.courseName << endl;
+    cout << "Course Marks: " << s1.courseMarks << endl;
+ return 0;
+}
